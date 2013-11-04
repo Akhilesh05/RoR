f124237d02fd1a6e0dd52a0283ee14f5243d4159
Girish Gopaul <akhile@dr.com>
2013-11-04 10:32:02 +0000

    Update micropost.rb

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,7 +5,7 @@ class Micropost < ActiveRecord::Base
   # # # # # Validations starts here # # # # #
   
   validates_length_of :content, :maximum => 140
-  validates_presence_of :content, :user_id, :user
+  validates_presence_of :content, :user_id, :user # :user will check whether the specified user exists in the users table
   validates_numericality_of :user_id, :only_integer => true
   
 end
