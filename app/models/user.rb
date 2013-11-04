38e0e81c264db69505e13b3926ec2316b87d91c7
Girish Gopaul <akhile@dr.com>
2013-11-04 10:16:44 +0000

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ActiveRecord::Base
   has_many :microposts
   
   #  #  #  #  #  #  #  Old way to do the same thing  #  #  #  #  #  #  #  #  #  #  #  #  #
+  #                                                                                      #
   #  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i  #
   #  validates_presence_of :email, :name                                                 #
+  #                                                                                      #
+  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #
   
   validates :email,  format: { with: }
   validates :email, :name, presence: true
