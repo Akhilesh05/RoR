b35e05d4904443b90f94c3c68b980bceea357423
Girish Gopaul <akhile@dr.com>
2013-11-04 10:27:22 +0000

    Update micropost.rb

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,7 +5,7 @@ class Micropost < ActiveRecord::Base
   # # # # # Validations starts here # # # # #
   
   validates_length_of :content, :maximum => 140
-  validates_presence_of :content, :user_id
+  validates_presence_of :content, :user_id, :user
   validates_numericality_of :user_id, :only_integer => true
   
 end
