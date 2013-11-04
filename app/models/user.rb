4c97b210cea764f8efdfa22f72f5f9063ca173f0
Girish Gopaul <akhile@dr.com>
2013-11-04 10:03:23 +0000

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   has_many :microposts
   
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates_presence_of :email, :name
   
 end
