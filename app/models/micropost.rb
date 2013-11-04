462f29fbc6bf0503a1b6b5f10887b49df4a94072
Girish Gopaul <akhile@dr.com>
2013-11-04 09:53:17 +0000

    Update micropost.rb

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,7 @@ class Micropost < ActiveRecord::Base
   
   belongs_to :user
   
-  validates :content, :length => { :maximum => 140 }
+  validates_length_of :content, :maximum => 140
+  validates_presence_of :content, :user_id
   
 end
