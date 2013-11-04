a3652dcbe79cf55bf0f6ac70dc30e06ffdfc84b6
Girish Gopaul <akhile@dr.com>
2013-11-04 10:48:36 +0000

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,11 @@ class User < ActiveRecord::Base
   #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #
   
   validates :email,  format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
-                               message: "Email address is not valid!!" }
+                               message: "Oops, address is not valid!!" }
                                
-  validates :email, :name, presence: { message: "
+  validates :email, :name, presence: { message: "Oops, looks like you've missed some important details" }
   
   validates :email, uniqueness: { case_sensitive: false,
-                                  message: %Q{Oops, looks like you've missed some important details} }
+                                  message: %Q{Oops, looks like this email is already taken} }
   
 end
